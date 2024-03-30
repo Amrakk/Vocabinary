6014d5fc03ccc5fe90a293670f2d2535c7c9a5f6
Amrakk <4mr4kk@gmail.com>
2024-03-30 17:42:24 +0700

    feat: update dictionary, imgbb response and env

---
+++ lib/models/api_responses/imgbb_api_res.dart
@@ -0,0 +1,15 @@
+class ImgbbResponse {
+  String? image;
+  String? thumb;
+  String? medium;
+
+  ImgbbResponse({this.image, this.thumb, this.medium});
+
+  factory ImgbbResponse.fromMap(Map<String, dynamic> map) {
+    return ImgbbResponse(
+      image: map['image']['url'] ?? '',
+      thumb: map['thumb']['url'] ?? '',
+      medium: map['medium']['url'] ?? '',
+    );
+  }
+}
